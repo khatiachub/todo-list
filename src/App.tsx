2d1b71bcf22b5152e101ccf9cafea3867da8284f
khatiachub <101997849+khatiachub@users.noreply.github.com>
2023-05-04 01:03:01 +0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      {/* <Reset/> */}
+      <Reset/>
       <Routes>
         <Route path="/"element={<Home/>}/>
         <Route path="/sign"element={<Sign/>}/>
